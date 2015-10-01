634c0f83d6811bc955ee937ad6e59f434b23361f
naufalafifhidayah <naufalafifh1996@gmail.com>
2015-10-01 17:21:41 +0700

    update simulation

---
+++ Aplikasi Warna/Aplikasi Warna/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Me.BackColor = Color.Orange Then
+
+            Me.BackColor = Color.Purple
+        Else
+            Me.BackColor = Color.DarkBlue
+
+
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.BackColor = Color.Orange
+    End Sub
+End Class
---
+++ Aplikasi Warna/Latihan 1/WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,5 @@
+﻿
+
+    Private Sub Label2_Click(sender As System.Object, e As System.EventArgs) Handles Label2.Click
+
+    End Sub
